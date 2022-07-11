9f4e8bc8d66c5cbbb384f76f8c45d1ca0ba9d9c6
levy002 <levy.ukwi002@gmail.com>
2022-07-11 16:07:12 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
  Rails.application.routes.draw do
    root 'users#index'
    resources :users, only: [:index, :show] do
-     resources :posts, only: [:index, :show] do
-    end
+     resources :posts, only: [:index, :show]
   end
 
