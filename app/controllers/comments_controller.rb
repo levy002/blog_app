4cb1d255207473ee93d01373455b86b28fa9c7d3
levy002 <levy.ukwi002@gmail.com>
2022-07-15 17:59:21 +0200

    Ficx rubocop errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,23 @@
 class CommentsController < ApplicationController
-    def new
-        @comment = Comment.new
-        @user = current_user
-    end
+  def new
+    @comment = Comment.new
+    @user = current_user
+  end
 
-    def create
-        user = current_user
-        post = current_post
-        comment = Comment.new(comment_params)
-        comment.author = user
-        comment.post = post
-        if comment.save
-            redirect_to user_post_url(id: post.id)
-        else
-            redirect_to new_user_post_comment_url(post_id: post.id, user_id: user.id)
-        end
+  def create
+    user = current_user
+    post = current_post
+    comment = Comment.new(comment_params)
+    comment.author = user
+    comment.post = post
+    if comment.save
+      redirect_to user_post_url(id: post.id)
+    else
+      redirect_to new_user_post_comment_url(post_id: post.id, user_id: user.id)
     end
+  end
 
-
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
