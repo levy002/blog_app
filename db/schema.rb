effd9a6d7a2639a23ebb4575a80aa6c7a958ae48
levy002 <levy.ukwi002@gmail.com>
2022-07-19 12:26:01 +0200

    Add new database

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_055538) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
